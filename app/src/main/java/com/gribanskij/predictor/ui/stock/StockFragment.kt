9e4153c71b314848a4df1df1024105604e3a8d5b
gribanskij <gribanskij1979@gmail.com>
2021-09-09 17:23:54 +0300

    210909 edit

--- app/src/main/java/com/gribanskij/predictor/ui/stock/StockFragment.kt
+++ app/src/main/java/com/gribanskij/predictor/ui/stock/StockFragment.kt
@@ -27,8 +27,8 @@ class StockFragment : Fragment(R.layout.fragment_stock) {
     private val binding get() = _binding!!
     private val model: StockViewModel by viewModels()
 
-    private val stockDataSet = mutableListOf<Entry>()
-    private val predictDataSet = mutableListOf<Entry>()
+    private val historyDataSet = mutableListOf<Stock>()
+    private val predictDataSet = mutableListOf<SimpleStock>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +48,6 @@ class StockFragment : Fragment(R.layout.fragment_stock) {
         _binding = FragmentStockBinding.bind(view)
 
         chartInit()
-
 
 
         model.updateStatus.observe(viewLifecycleOwner, {
@@ -86,18 +85,20 @@ class StockFragment : Fragment(R.layout.fragment_stock) {
 
             when (it) {
                 is Result.Success -> {
-                    //binding.progress.visibility = View.INVISIBLE
-                    showData(it.data)
+                    historyDataSet.clear()
+                    historyDataSet.addAll(it.data)
+                    if (binding.dataToggle.isChecked) {
+                        binding.chartProgress.visibility = View.INVISIBLE
+                        showHistoryDataChart(historyDataSet)
+                    }
                 }
                 is Result.Error -> {
-                    //binding.progress.visibility = View.INVISIBLE
+                    binding.chartProgress.visibility = View.INVISIBLE
                 }
                 is Result.Loading -> {
-                    //binding.progress.visibility = View.VISIBLE
+                    binding.chartProgress.visibility = View.VISIBLE
                 }
             }
-
-
         })
 
         model.predictData.observe(viewLifecycleOwner, {
@@ -106,48 +107,72 @@ class StockFragment : Fragment(R.layout.fragment_stock) {
 
                 is Result.Success -> {
                     predictDataSet.clear()
-
-                    for ((i, element) in it.data.withIndex()) {
-                        val predictItem = Entry(i.toFloat(), element.value)
-                        predictDataSet.add(predictItem)
+                    predictDataSet.addAll(it.data)
+                    if (!binding.dataToggle.isChecked) {
+                        binding.chartProgress.visibility = View.INVISIBLE
+                        showPredictDataChart(predictDataSet)
                     }
-                    val label = arguments?.getString(ARG_STOCK_NAME, "?")
-
-                    val pDataSet = LineDataSet(predictDataSet, label)
-                    pDataSet.color = R.color.teal_200
-                    pDataSet.setCircleColor(R.color.teal_200)
-
-                    //binding.modelChart.xAxis.valueFormatter = PredictXAxisFormatter(it.data)
-                    //binding.modelChart.data = LineData(pDataSet)
-                    //binding.modelChart.invalidate()
-
                 }
-                else -> {
-
+                is Result.Error -> {
+                    binding.chartProgress.visibility = View.INVISIBLE
+                }
+                is Result.Loading -> {
+                    binding.chartProgress.visibility = View.VISIBLE
                 }
             }
 
         })
+
+        binding.dataToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                showHistoryDataChart(historyDataSet)
+            } else {
+                showPredictDataChart(predictDataSet)
+            }
+        }
     }
 
 
-    private fun showData(data: List<Stock>) {
-        stockDataSet.clear()
+    private fun showHistoryDataChart(data: List<Stock>) {
+
+
+        val dataSet = mutableListOf<Entry>()
+
         for ((i, element) in data.withIndex()) {
             val item = Entry(i.toFloat(), element.priceClose)
-            stockDataSet.add(item)
+            dataSet.add(item)
         }
 
         val label = arguments?.getString(ARG_STOCK_NAME, "?")
-        val lDataSet = LineDataSet(stockDataSet, label)
+        val lDataSet = LineDataSet(dataSet, label)
         lDataSet.color = R.color.black
         lDataSet.setCircleColor(R.color.black)
-
 
         binding.dataChart.xAxis.valueFormatter = StockXAxisFormatter(data)
         binding.dataChart.data = LineData(lDataSet)
         binding.dataChart.invalidate()
 
+    }
+
+    private fun showPredictDataChart(data: List<SimpleStock>) {
+
+
+        val dataSet = mutableListOf<Entry>()
+
+        for ((i, element) in data.withIndex()) {
+            val item = Entry(i.toFloat(), element.value)
+            dataSet.add(item)
+        }
+
+        val label = arguments?.getString(ARG_STOCK_NAME, "?")
+        val lDataSet = LineDataSet(dataSet, label)
+
+        lDataSet.color = R.color.teal_200
+        lDataSet.setCircleColor(R.color.teal_200)
+
+        binding.dataChart.xAxis.valueFormatter = PredictXAxisFormatter(data)
+        binding.dataChart.data = LineData(lDataSet)
+        binding.dataChart.invalidate()
 
     }
 
