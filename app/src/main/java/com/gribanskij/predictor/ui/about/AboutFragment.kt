c0db4c35ed0b26803a9a65429a4b77f605f6b221
gribanskij <gribanskij1979@gmail.com>
2021-10-04 14:53:19 +0300

    211004 about

--- app/src/main/java/com/gribanskij/predictor/ui/about/AboutFragment.kt
+++ app/src/main/java/com/gribanskij/predictor/ui/about/AboutFragment.kt
@@ -1,31 +1,27 @@
 package com.gribanskij.predictor.ui.about
 
+
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.gribanskij.predictor.BuildConfig
 import com.gribanskij.predictor.R
+import com.gribanskij.predictor.databinding.FragmentAboutBinding
 
-class AboutFragment : Fragment() {
+class AboutFragment : Fragment(R.layout.fragment_about) {
 
-    private lateinit var galleryViewModel: AboutViewModel
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(AboutViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_about, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentAboutBinding.bind(view)
+        binding.versionName.text = BuildConfig.VERSION_NAME
+
     }
 }
